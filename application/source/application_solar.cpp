cdbef763ebc3864d8bc409a17201a9208f425fa8
Anton Lammert <antonbenlammert@gmail.com>
2019-12-08 23:25:56 +0100

    Added orbits to the solarsystem. Created two simple shader for this purpose.

--- application/source/application_solar.cpp
+++ application/source/application_solar.cpp
@@ -42,6 +42,10 @@ ApplicationSolar::~ApplicationSolar() {
   glDeleteBuffers(1, &stars_object.vertex_BO);
   glDeleteBuffers(1, &stars_object.element_BO);
   glDeleteVertexArrays(1, &stars_object.vertex_AO);
+
+  glDeleteBuffers(1, &orbits_object.vertex_BO);
+  glDeleteBuffers(1, &orbits_object.element_BO);
+  glDeleteVertexArrays(1, &orbits_object.vertex_AO);
 }
 
 void ApplicationSolar::initializeSceneGraph() {
@@ -160,10 +164,18 @@ void ApplicationSolar::uploadView() {
   glUniformMatrix4fv(m_shaders.at("stars").u_locs.at("ModelViewMatrix"),
   					 1, GL_FALSE, glm::value_ptr(view_matrix));
 
+  
   glUseProgram(m_shaders.at("planet").handle);
 
   glUniformMatrix4fv(m_shaders.at("planet").u_locs.at("ViewMatrix"),
                      1, GL_FALSE, glm::value_ptr(view_matrix));
+
+
+  glUseProgram(m_shaders.at("orbits").handle);
+
+  // upload matrix to gpu
+  glUniformMatrix4fv(m_shaders.at("orbits").u_locs.at("ViewMatrix"),
+	  1, GL_FALSE, glm::value_ptr(view_matrix));
 }
 
 void ApplicationSolar::uploadProjection() {
@@ -179,6 +191,11 @@ void ApplicationSolar::uploadProjection() {
 
   glUniformMatrix4fv(m_shaders.at("planet").u_locs.at("ProjectionMatrix"),
                      1, GL_FALSE, glm::value_ptr(m_view_projection));
+
+  glUseProgram(m_shaders.at("orbits").handle);
+
+  glUniformMatrix4fv(m_shaders.at("orbits").u_locs.at("ProjectionMatrix"),
+	  1, GL_FALSE, glm::value_ptr(m_view_projection));
 }
 
 // update uniform locations
@@ -207,21 +224,31 @@ void ApplicationSolar::initializeShaderPrograms() {
   // request uniform locations for shader program
   m_shaders.at("stars").u_locs["ModelViewMatrix"] = -1;
   m_shaders.at("stars").u_locs["ProjectionMatrix"] = -1;
+
+
+  // store orbit shader
+  m_shaders.emplace("orbits", shader_program{{{GL_VERTEX_SHADER,m_resource_path + "shaders/orb.vert"},
+										    {GL_FRAGMENT_SHADER, m_resource_path + "shaders/orb.frag"}}});
+
+  // request uniform locations for shader program
+  m_shaders.at("orbits").u_locs["ModelMatrix"] = -1;
+  m_shaders.at("orbits").u_locs["ViewMatrix"] = -1;
+  m_shaders.at("orbits").u_locs["ProjectionMatrix"] = -1;
 }
 
 void ApplicationSolar::initializeStars(){
 
 	std::vector<GLfloat> stars;
-	uint const numberStars = 1000;
+	unsigned int const numberStars = 10000;
 	stars.reserve(numberStars*6*sizeof(float));
 
-	for(uint i =0; i<numberStars; i++)
+	for(unsigned int i =0; i<numberStars; i++)
 	{
-		float rand_x = static_cast<float>(std::rand() % 41) - 20;
+		float rand_x = static_cast<float>(std::rand() % 4100)/100.0f - 20;
 		stars.push_back(rand_x);
-		float rand_y = static_cast<float>(std::rand() % 41) - 20;
+		float rand_y = static_cast<float>(std::rand() % 4100)/100.0f - 20;
 		stars.push_back(rand_y);
-		float rand_z = static_cast<float>(std::rand() % 41) - 20;
+		float rand_z = static_cast<float>(std::rand() % 4100)/100.0f - 20;
 		stars.push_back(rand_z);
 		float rand_r = static_cast<float>(std::rand()%255)/255.0f;
 		stars.push_back(rand_r);
@@ -291,16 +318,78 @@ void ApplicationSolar::initializePlanets(){
 }
 
 void ApplicationSolar::initializeOrbits(){
+	auto root = scenegraph_->getRoot();
+	//specify the number of planets and the number of points per orbit aswell as the angle between each of these points
+	int num_planets = 10;
+	int num_orbit_points = 65;
+	float angle = 0.1f;
+	//specify the names of plante nodes that should get an orbit
+	std::string planets[] = {
+	"sun_geom",
+	"uran_geom",
+	"venus_geom",
+	"earth_geom",
+	"moon_geom",
+	"merc_geom",
+	"mars_geom",
+	"jupit_geom",
+	"sat_geom",
+	"nept_geom" };
+	
+	std::vector<GLfloat> orbit;
+	//for each planet calculate the position of the points on the orbit
+	for (auto pl : planets) {
+		orbit.clear();
+		auto planet_geom = root->getChildren(pl);
+		auto planet_hold = planet_geom->getParent();
+		//create a rotation matrix to calculate the points on the orbit
+		auto rot_mat = glm::rotate(glm::mat4x4{}, angle, glm::fvec3{ 0.0f,1.0f,0.0f });
+		auto point = planet_hold->getLocalTransform()*glm::fvec4{ 0.0f,0.0f,0.0f,1.0f};
+		//push the new calculated points nack
+		for (int i = 0; i < num_orbit_points; ++i) {
+			orbit.push_back(point.x);
+			orbit.push_back(point.y);
+			orbit.push_back(point.z);
+			point = rot_mat * point;
+		}
+		//create a model using the vertices of the orbit
+		model orbit_model{};
+		orbit_model.data = orbit;
+		orbit_model.vertex_num = num_orbit_points;
+		//create a new geometry node using the orbit model
+		auto orbit_node = std::make_shared<GeometryNode>(pl + "_orbit", orbit_model);
+		//add the new node to the scenegraph
+		planet_hold->getParent()->addChildren(orbit_node);
+		orbit_node->setParent(planet_hold->getParent());
+	}
 
+	//create a new VertexArray
+	glGenVertexArrays(1, &orbits_object.vertex_AO);
+	glBindVertexArray(orbits_object.vertex_AO);
+
+	//generate a new Buffer and bind it to the new VertexArray
+	glGenBuffers(1, &orbits_object.vertex_BO);
+	glBindBuffer(GL_ARRAY_BUFFER, orbits_object.vertex_BO);
+	//specify the size of the data
+	glBufferData(GL_ARRAY_BUFFER, sizeof(float)*orbit.size(), orbit.data(), GL_STATIC_DRAW);
+
+	// first attribArray for positions
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, GLsizei(3 * sizeof(float)), 0);
+
+	//specify the draw mode and the number of elements
+	orbits_object.draw_mode = GL_LINE_STRIP;
+	orbits_object.num_elements = GLsizei(num_orbit_points);
 }
 
 void ApplicationSolar::render()const{
 
 	renderStars();
 	renderPlanets();
+	renderOrbits();
 }
 
-void ApplicationSolar::renderStars()const{
+void ApplicationSolar::renderStars() const{
 
 	glUseProgram(m_shaders.at("stars").handle);	
 
@@ -368,9 +457,46 @@ void ApplicationSolar::renderPlanets()const{
 }
 
 void ApplicationSolar::renderOrbits()const{
+	//get the orbit nodes by name
+	std::string orbits[] = {
+	"sun_geom_orbit",
+	"uran_geom_orbit",
+	"venus_geom_orbit",
+	"earth_geom_orbit",
+	"moon_geom_orbit",
+	"merc_geom_orbit",
+	"mars_geom_orbit",
+	"jupit_geom_orbit",
+	"sat_geom_orbit",
+	"nept_geom_orbit" };
 
-	auto root = scenegraph_->getRoot();
+	//declare the shader we want to use
+	glUseProgram(m_shaders.at("orbits").handle);
 
+	//for every orbit of a planet draw it
+	for (auto& orbit_name : orbits) {
+
+		//get the geometry of the orbit that is stored in the node
+		auto orbit = scenegraph_->getRoot()->getChildren(orbit_name);
+		auto orbit_geom = std::static_pointer_cast<GeometryNode> (orbit);
+		auto orbit_world_transform = orbit->getWorldTransform();
+		model orbit_model = orbit_geom->getGeometry();
+		std::vector<GLfloat> orbit_data = (*orbit_geom).getGeometry().data;
+		
+		//create the ModelMatrix using the WorldTransform of the orbit
+		glUniformMatrix4fv(m_shaders.at("orbits").u_locs.at("ModelMatrix"),
+			1, GL_FALSE, glm::value_ptr(orbit_world_transform));
+		
+		//bind the buffer of the orbit to set the data of the current orbit
+		glBindBuffer(GL_ARRAY_BUFFER, orbits_object.vertex_BO);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(float)*orbit_data.size(), orbit_data.data(), GL_STATIC_DRAW);
+		
+		//bind the VertexArray for drawing
+		glBindVertexArray(orbits_object.vertex_AO);
+		
+		//draw the array
+		glDrawArrays(orbits_object.draw_mode, GLint(0), orbits_object.num_elements);
+	}
 }
 
 ///////////////////////////// callback functions for window events ////////////
